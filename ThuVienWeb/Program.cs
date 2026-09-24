a28f3d1c024d995e70676dea1fc38b18c747243e
hovin <hovin@HOVINHQUANG>
2026-09-24 15:59:43 +0700

    phần 4

--- ThuVienWeb/Program.cs
+++ ThuVienWeb/Program.cs
@@ -1,23 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using ThuVienWeb.Data;
+using ThuVienWeb.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 //register DB
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IBookRepository, SQLBookRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwaggerUI();
+    app.UseSwagger();
 }
 
 app.UseHttpsRedirection();
---
+++ ThuVienWeb/Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using ThuVienWeb.Models.Domain;
+using ThuVienWeb.Models.DTO;
+
+namespace ThuVienWeb.Repositories
+{
+    public interface IBookRepository
+    {
+        List<BookWithAuthorAndPublisherDTO> GetAllBooks();
+        BookWithAuthorAndPublisherDTO GetBookById(int id);
+        AddBookRequestDTO AddBook(AddBookRequestDTO addBookRequestDTO);
+        AddBookRequestDTO? UpdateBookById(int id, AddBookRequestDTO bookDTO);
+        Books? DeleteBookById(int id);
+    }
+
+}
